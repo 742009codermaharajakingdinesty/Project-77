dc7318a6dbdd593da9c42c4c432030d474b40bab
742009codermaharajakingdinesty <97498655+742009codermaharajakingdinesty@users.noreply.github.com>
2022-01-11 10:55:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var first_name="Mahir";
+var last_name="Dhasmana";
+var full_name=first_name.concat(last_name);
+console.log(full_name);
+
+var a=10;
+var b=5;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
